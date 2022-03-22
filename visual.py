0251725baf42f110f756c664b33b201819018887
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2022-03-22 23:39:50 +0100

    Visualising the neural network

--- visual.py
+++ visual.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation 
 import numpy as np 
 
-#plt.style.use('dark_background')
 
 fig = plt.figure() 
 ax = plt.axes(xlim=(-50, 50), ylim=(-50, 50)) 
